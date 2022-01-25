45bee18ae927ce3253bc2918845906a9a32ea20d
Will Neal <willwneal@gmail.com>
2022-01-25 17:20:52 -0500

    used array for question and answers instead of hardcoding. Improved visual

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,37 @@
 console.log("If you're reading this I am linked")
 //Questions 
+
+//function to remove all child nodes of a parent element
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-var questionOne = {
-    question: "test123",
-    choiceOne: "a",
-    choiceTwo: "b",
-    choiceThree: "c",
+var questionIndex = 0
+
+var questionArray = [{
+    question: "Which of the following is a framework?",
+    choiceOne: "A. CSS",
+    choiceTwo: "B. Bootstrap",
+    choiceThree: "C. JavaScript",
+    choiceFour: "D. MySQL",
+
+}, {
+    question: "question 2?",
+    choiceOne: "1",
+    choiceTwo: "2",
+    choiceThree: "3",
+    choiceFour: "4",
 }
+]
 
 
 
 //variables
 var startButton = document.querySelector("#start");
 var quizChange = document.querySelector(".quizWindow");
-var question = document.createElement("h2")
+var question = document.createElement("div")
 var choiceA = document.createElement("button")
 var choiceB = document.createElement("button")
 var choiceC = document.createElement("button")
@@ -26,24 +39,37 @@ var choiceD = document.createElement("button")
 var timer = document.createElement("div")
 
 
+var numCorrect = 0
+var numIncorrect = 0
 //START THE GAME
 startButton.addEventListener("click", function(event){
     console.log("you clicked the button");
+    //This brings up the first question of the quiz
     removeAllChildNodes(quizChange);
-    //quizChange.remove()
     quizChange.appendChild(question)
-    quizChange.setAttribute("id", "quizQuestions")
-    quizChange.textContent = "This is the first question. Choose one of the four answers: "
+    question.setAttribute("id", "quizQuestions")
+    question.textContent = questionArray[questionIndex].question;
     quizChange.appendChild(choiceA);
+    choiceA.setAttribute("class", "answer");
     choiceA.setAttribute("id", "choiceA");
-    choiceA.textContent = "I am the first answer"
+    choiceA.textContent = questionArray[questionIndex].choiceOne;
     quizChange.appendChild(choiceB);
+    choiceB.setAttribute("class", "answer");
     choiceB.setAttribute("id", "choiceB");
-    choiceB.textContent = "I am the second answer"
+    choiceB.textContent = questionArray[questionIndex].choiceTwo;
     quizChange.appendChild(choiceC);
+    choiceC.setAttribute("class", "answer");
     choiceC.setAttribute("id", "choiceC");
-    choiceC.textContent = "I am the third answer"
+    choiceC.textContent = questionArray[questionIndex].choiceThree;
     quizChange.appendChild(choiceD);
+    choiceD.setAttribute("class", "answer");  
     choiceD.setAttribute("id", "choiceD");
-    choiceD.textContent = "I am the fourth answer"
+    choiceD.textContent = questionArray[questionIndex].choiceFour;
+    //moves on to next questions and tallies correct or incorrect answers.
+
+    var answerClick = document.querySelector(".answer")
+    answerClick.addEventListener("click", function(event){
+        console.log("you have clicked an answer")
+    })
+
 })
